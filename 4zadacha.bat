bce86249655bef73a2e210c55fe5d938ae1b9ac7
Olya178 <sumakovaolga53@gmail.com>
2024-10-11 14:46:30 +0300

    laba3

---
+++ 4zadacha.bat
@@ -0,0 +1,6 @@
+@Echo OFF
+set /P mesto1=enter papka1
+set /P mesto2=enter papka2
+xcopy %mesto1% %mesto2% /S
+pause 
+
---
+++ privazka.bat
@@ -0,0 +1,4 @@
+@Echo off
+git config --global user.name "Olya178"
+git config --global user.email "sumakovaolga53@gmail.com"
+pause
